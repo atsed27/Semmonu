2f811832eae17c8a9ab3d2b752de3a8d35239969
atsed27 <danielnigatu09@gmail.com>
2023-12-26 05:04:57 -0300

    firstcommit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Next js e-commerce web application -> Kdame gbiya
---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import '@/styles/globals.css';
+
+import { SessionProvider } from 'next-auth/react';
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      {Component.auth ? (
+        <Component {...pageProps} />
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </SessionProvider>
+  );
+}
