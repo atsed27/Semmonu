320b78bdfab6f61e30b8625245ef46d476750ea4
atsed27 <danielnigatu09@gmail.com>
2023-12-29 06:02:28 -0300

    sign up end Api is done

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,23 @@
+import User from '@/model/User';
+import db from '@/utils/db';
+import bcrypt from 'bcryptjs';
+
+const signup = async (req, res) => {
+  if (req.method === 'POST') {
+    try {
+      await db.connect();
+      const findUser = await User.findOne({ email: req.body.email });
+      if (findUser) return res.status(404).json('email is already taken');
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(req.body.password, salt);
+      const newUser = new User({ ...req.body, password: hash });
+      await newUser.save();
+      await db.disconnect();
+      res.status(200).json('ok');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+export default signup;
