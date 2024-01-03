ddf5c7b116e364eb0b7493a6215a0ad90e68623b
atsed27 <danielnigatu09@gmail.com>
2024-01-03 06:26:34 -0300

    unauthorized page is add

--- pages/unauthorized.js
+++ pages/unauthorized.js
@@ -1,13 +1,22 @@
 import Layout from '@/components/Layout';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect } from 'react';
+import Link from 'next/link';
 
 function Unauthorized() {
   const router = useRouter();
   const { message } = router.query;
+  useEffect(() => {
+    if (message === 'login required') {
+      router.push('/login');
+    }
+  }, [message, router]);
   return (
     <Layout title={'unauthorized'}>
-      <div className="flex flex-col items-center justify-center h-screen">
+      <div className="px-4 mt-4">
+        <Link href={'/'}>back</Link>
+      </div>
+      <div className="flex flex-col items-center justify-center ">
         <h1 className="text-xl ">Access Denied</h1>
         {message && <div className="text-red-500">{message}</div>}
       </div>
