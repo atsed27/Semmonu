8c55997f1cd4705573c75f325826104941226180
atsed27 <danielnigatu09@gmail.com>
2024-01-15 07:21:11 -0300

    verify chapa payment is done

---
+++ pages/api/pay/chapa/verify/[id].js
@@ -0,0 +1,48 @@
+import db from '@/utils/db';
+import axios from 'axios';
+import User from '../../../../../model/User';
+
+const chapaVerification = async (req, res) => {
+  if (req.method === 'GET') {
+    try {
+      await db.connect();
+
+      let tx_ref = req.query.id;
+      console.log(tx_ref);
+      const option = {
+        headers: {
+          Authorization: process.env.chapa_id,
+          'Content-Type': 'application/json',
+        },
+      };
+      await axios
+        .get(`https://api.chapa.co/v1/transaction/verify/${tx_ref}`, option)
+        .then(async (response) => {
+          console.log(response.data);
+          const userFind = await User.findOne({ tx_ref: tx_ref });
+          if (!userFind) return res.status(404).json('user is not found');
+          const user = await User.findByIdAndUpdate(
+            userFind._id,
+            {
+              $set: {
+                userType: 'basic',
+              },
+            },
+            {
+              new: true,
+            }
+          );
+          await db.disconnect();
+          console.log(user);
+          res.send(user);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+export default chapaVerification;
